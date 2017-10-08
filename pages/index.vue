0e7c2fe4e75ea23d31c81a194c50141b1bf25d78
Nicholas Carr <nicholas.carr@rogers.com>
2017-10-07 20:45:08 -0400

    Changed branding
    Trying to trigger a build on Travis

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Cursor</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
@@ -354,7 +354,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-4">
-            <span class="copyright">Copyright &copy; Your Website 2017</span>
+            <span class="copyright">Copyright &copy; Cursor 2017</span>
           </div>
           <div class="col-md-4">
             <ul class="list-inline social-buttons">
