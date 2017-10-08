4d6d9c11b1fd2b1ac72080f13ffb4f63b69aefb0
Nicholas Carr <nicholas.carr@rogers.com>
2017-10-08 13:51:19 -0400

    Relied more on first-party modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,18 +5,16 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Cursor',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Website for Cursor' }
-    ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,700|Droid+Serif:400,700,400italic,700italic|Roboto+Slab:400,100,300,700' }
     ]
   },
-  plugins: [{ src: 'bootstrap', ssr: false }, { src: 'font-awesome-webpack', ssr: false },  { src: '~/plugins/theme', ssr: false }],
+  manifest: {
+    name: 'Cursor',
+    short_name: 'Cursor'
+  },
+  plugins: [{ src: 'bootstrap', ssr: false },  { src: '~/plugins/theme', ssr: false }],
+  modules: ['@nuxtjs/font-awesome', '@nuxtjs/icon', '@nuxtjs/meta', '@nuxtjs/manifest'],
   css: [
     '~/assets/style/theme.css',
     '~/assets/style/bootstrap.scss'
@@ -29,7 +27,7 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    vendor: ['jquery', 'popper.js', 'bootstrap', 'font-awesome-webpack'],
+    vendor: ['jquery', 'popper.js', 'bootstrap'],
     postCSS: [
       require('precss'),
       require('autoprefixer')
