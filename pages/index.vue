e93c5c3927524a4a3b73bbb74d08461a90ce0953
Sam V <samyar.vahid@gmail.com>
2017-10-20 18:47:14 -0400

    added profile pic for samyar

--- pages/index.vue
+++ pages/index.vue
@@ -137,7 +137,7 @@
         <div class="row">
           <div class="col-sm-4">
             <div class="team-member">
-              <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
+              <img class="mx-auto rounded-circle" src="~/static/humans/samyar.jpg" alt="">
               <h4>Samyar Vahid</h4>
               <p class="text-muted">Head of Marketing</p>
               <ul class="list-inline social-buttons">
