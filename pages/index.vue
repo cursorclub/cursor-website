e50799e9cd1a3f28fe3b56b0d13b4f637daf1292
Misha Larionov <MishaLarionov@users.noreply.github.com>
2017-10-26 18:53:22 -0400

    Update events description to better describe us

--- pages/index.vue
+++ pages/index.vue
@@ -59,7 +59,7 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading">Events</h2>
-            <h3 class="section-subheading text-muted">We host unique workshops, hackathons and more to help build our local tech community.</h3>
+            <h3 class="section-subheading text-muted">We're working hard on getting our first event off the ground. Stay tuned for more details!</h3>
           </div>
         </div>
         <div class="row text-center">
